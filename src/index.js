f46f81ee24ae8573a4bdd900abc246a65871f78c
shahadat <shahadat@tikweb.com>
2022-12-22 19:46:37 +0600

    feat: paginating and getting all data

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer')
 const cheerio = require('cheerio')
 const log = require('./logger')
 const crawler = require('./crawler')
-// const storage = require('./filesystem')
+const storage = require('./filesystem')
 
 const baseURL =
 	'https://www.otomoto.pl/ciezarowe/uzytkowe/mercedes-benz/od-2014/q-actros?search%5Bfilter_enum_damaged%5D=0&search%5Border%5D=created_at%3Adesc'
@@ -18,23 +18,39 @@ const baseURL =
 		await page.goto(baseURL)
 		log.info('went to URL')
 
-		const pageData = await page.evaluate(() => {
+		let pageData = await page.evaluate(() => {
 			return document.documentElement.innerHTML
 		})
 		log.info('HTML parsed')
 
-		const $ = cheerio.load(pageData)
+		let $ = cheerio.load(pageData)
 		log.info('HTML loaded in cheerio')
 
-		// const items = crawler.addItems($)
-		// log.info('Scrapped list of items')
-		// storage.updateJson('items.json', items)
-
 		const lastPage = crawler.findLastPage($)
-		log.info(`last page: ${lastPage}`)
+		log.info(`pagination last page: ${lastPage}`)
 
-		const count = crawler.getTotalAdsCount($)
-		log.info(`got total ads count: ${count}`)
+		let totalCount = 0
+
+		// * paginate all pages
+		for (let index = 1; index <= lastPage; index++) {
+			await page.goto(baseURL + `&page=${index}`)
+
+			pageData = await page.evaluate(() => {
+				return document.documentElement.innerHTML
+			})
+
+			$ = cheerio.load(pageData)
+
+			const items = crawler.addItems($)
+			log.info(`Scrapped Data from Page: ${index}`)
+			storage.updateJson('items.json', items)
+
+			const count = crawler.getTotalAdsCount($)
+			log.info(`ads count: ${count}`)
+			totalCount = totalCount + count
+		}
+
+		log.info(`got total ads count: ${totalCount}`)
 	} catch (error) {
 		log.warn(error.message)
 		log.info(error)
